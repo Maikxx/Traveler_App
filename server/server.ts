d211457465cd4b44c810f300539bfb9451a1732f
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-07 20:30:26 +0100

    feat(*): add several routes

--- server/server.ts
+++ server/server.ts
@@ -11,10 +11,40 @@ app.use(express.static('public'))
 
 // Routes
 app.get('/', renderIndex)
+app.get('/chat', renderChat)
+app.get('/chats', renderChats)
+app.get('/match_profile', renderMatchProfile)
+app.get('/matches_overview', renderMatchesOverview)
+app.get('/my_profile', renderMyProfile)
+app.get('/questionaire', renderQuestionaire)
 
 // Serving
 app.listen(8000)
 
 function renderIndex (req: any, res: any) {
-    res.render('index.ejs');
+    res.render('index.ejs')
+}
+
+function renderChat (req: any, res: any) {
+    res.render('chat.ejs')
+}
+
+function renderChats (req: any, res: any) {
+    res.render('chats.ejs')
+}
+
+function renderMatchProfile (req: any, res: any) {
+    res.render('match_profile.ejs')
+}
+
+function renderMatchesOverview (req: any, res: any) {
+    res.render('matches_overview.ejs')
+}
+
+function renderMyProfile (req: any, res: any) {
+    res.render('my_profile.ejs')
+}
+
+function renderQuestionaire (req: any, res: any) {
+    res.render('questionaire.ejs')
 }
