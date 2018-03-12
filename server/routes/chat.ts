5b37bf4b4bacac3cec8cd6514ad4656803308d99
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-12 12:13:21 +0100

    feat(chatView): add variables

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -1,5 +1,44 @@
 function renderChat (req: any, res: any) {
-    res.render('chat.ejs')
+    const chat = {
+        chatId: '1',
+        chatWithName: 'Henk',
+        chatWithId: '1',
+        messages: [
+            {
+                messageId: '1',
+                sentByWho: 'me',
+                profileImageUrl: '/img/available-traveler.jpg',
+                sentBy: 'Henk',
+                messageText: 'Hello there!',
+            },
+            {
+                messageId: '2',
+                sentByWho: 'them',
+                profileImageUrl: '/img/available-traveler.jpg',
+                sentBy: 'Me',
+                messageText: 'Hello!',
+            },
+            {
+                messageId: '3',
+                sentByWho: 'me',
+                profileImageUrl: '/img/available-traveler.jpg',
+                sentBy: 'Henk',
+                messageText: 'Hello thero!',
+            },
+            {
+                messageId: '4',
+                sentByWho: 'them',
+                profileImageUrl: '/img/available-traveler.jpg',
+                sentBy: 'Me',
+                messageText: 'Hellooo!',
+            },
+        ],
+        nextMessageId: '5',
+    }
+
+    res.render('chat.ejs', {
+        chat: chat,
+    })
 }
 
 export default renderChat
