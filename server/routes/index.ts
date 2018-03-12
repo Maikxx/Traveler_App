6558b06625d60a1b5d80b25435c06641b515851d
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-12 12:47:47 +0100

    feat(index): add templating

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,22 @@
 function renderIndex (req: any, res: any) {
-    res.render('index.ejs')
+    const availableTravelersData = [
+        {
+            id: '1',
+            name: 'Henk Sneevliet',
+            imageUrl: '/img/available-traveler.jpg',
+            profileDescription: 'Henk loves to travel, just like you, otherwise you wouldn\'t be here, right?',
+        },
+        {
+            id: '2',
+            name: 'Frank Fransen',
+            imageUrl: '/img/available-traveler.jpg',
+            profileDescription: 'Frank loves to travel, just like you, otherwise you wouldn\'t be here, right?',
+        },
+    ]
+
+    res.render('index.ejs', {
+        availableTravelersData,
+    })
 }
 
 export default renderIndex
