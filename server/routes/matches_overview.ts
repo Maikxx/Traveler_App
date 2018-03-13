f42176ddc6ae4b63f0c7e264082d09cb83fcd133
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-13 22:45:12 +0100

    feat(matchesOverview): add sample data and fix some styling

--- server/routes/matches_overview.ts
+++ server/routes/matches_overview.ts
@@ -1,5 +1,30 @@
 function renderMatchesOverview (req: any, res: any) {
-    res.render('matches_overview.ejs')
+    const overviewData = [
+        {
+            profileId: '1',
+            firstName: 'Henk',
+            fullName: 'Henk Smits',
+            profileImage: '/img/available-traveler.jpg',
+            age: 31,
+            description: 'Henk loves to travel, just like you, otherwise you wouldn\'t be here, right?',
+            favouriteHolidayDestination: 'Dubai',
+            likesToHike: 'Yes',
+            favouriteOverallTravelTime: 3,
+        },
+        {
+            profileId: '2',
+            firstName: 'Henry',
+            fullName: 'Henry Smits',
+            profileImage: '/img/available-traveler.jpg',
+            age: 31,
+            description: 'Henry loves to travel, just like you, otherwise you wouldn\'t be here, right?',
+            favouriteHolidayDestination: 'Dubai',
+            likesToHike: 'No',
+            favouriteOverallTravelTime: 5,
+        },
+    ]
+
+    res.render('matches_overview.ejs', { overviewData })
 }
 
 export default renderMatchesOverview
