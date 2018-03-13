2a2e21d3404fc02861bf43cc319fdcfe441a78f4
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-13 09:55:04 +0100

    feat(index.ejs): add button to sign in below sign up

--- public/index.js
+++ public/index.js
@@ -8,3 +8,20 @@ const handleMenuClick = (() => {
         })
     }
 })()
+
+const handleToggleSignInClick = (() => {
+    const toggleSignIn = document.getElementById('toggle-sign-in-button')
+    const navigation = document.getElementById('tl-Navigation')
+
+    if (toggleSignIn && navigation) {
+        toggleSignIn.addEventListener('click', function () {
+
+            window.location.hash === '#tl-Navigation' ? (
+                window.location.hash = '',
+                window.location.hash = 'tl-Navigation'
+            ) : window.location.hash = 'tl-Navigation'
+
+            navigation.classList.toggle('nav-toggled')
+        })
+    }
+})()
