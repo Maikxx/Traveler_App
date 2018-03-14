a59e855cfaa9f4e2c53471bbde8a515e33c3aee6
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-14 12:26:35 +0100

    feat(*): add a lot of data implementations

--- server/routes/my_profile_edit.ts
+++ server/routes/my_profile_edit.ts
@@ -1,5 +1,37 @@
 function renderMyProfileEdit (req: any, res: any) {
-    res.render('my_profile_edit.ejs')
+    const profileData = {
+        profileId: 1,
+        firstName: 'Sandra',
+        fullName: 'Sandra de Doorn',
+        birthDate: '1980/01/01',
+        age: 29,
+        profileImage: '/img/available-traveler.jpg',
+        description: 'Henk loves to travel, just like you, otherwise you wouldn\'t be here, right?',
+        hasTraveledTo: [ 'Amsterdam', 'Berlin' ].join(', '),
+        favouriteHolidayDestination: 'Dubai',
+        favouriteHolidayTypes: [ 'Backpacking', 'Family' ],
+        plansHolidaysAhead: 'Sometimes',
+        likesToHike: 'Yes',
+        prefersInterContinental: 'Yes',
+        wantsToVisitSoon: [ 'Dubai', 'Paris' ].join(', '),
+        hasVisitedThisMuchDestinations: 20,
+        favouriteOverallTravelTime: 3,
+        wantsToTravelQuickly: 'Yes',
+        mostImportantInRelationShip: 'Trust',
+        wantsToMarry: 'Yes',
+        foremostRelationshipMotivation: 'Being able to travel the world with that someone',
+        wantsToOrAlreadyHasChildren: 'Yes, I have them',
+        drinksAlcohol: 'Yes, often',
+        smokes: 'No',
+        likesToBeInNature: 'Yes',
+        favouriteMusicGenre: 'Hardstyle',
+        yearlyEarns: 'More than 50K',
+        livesIn: 'Rotterdam, the Netherlands',
+        jobTitle: 'Architect',
+        lengthInCm: 183,
+    }
+
+    res.render('my_profile_edit.ejs', { profileData })
 }
 
 export default renderMyProfileEdit
