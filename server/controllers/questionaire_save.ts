611cfc19194b37c9353e27ea618ae070de11a4ac
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-19 10:41:38 +0100

    feat(questionaire): add support for saving data to your profile

--- server/controllers/questionaire_save.ts
+++ server/controllers/questionaire_save.ts
@@ -1,13 +1,9 @@
 import Profile from '../models/profile'
 import handleHTTPError from './handle_error'
+import getAge from '../utils/getAge'
 
 function handleQuestionaireSave (req: any, res: any) {
     const { _id } = req.params
-
-    const queryData = {
-        matchSettings: {},
-        age: null,
-    }
 
     const {
         hasTraveledTo,
@@ -40,115 +36,134 @@ function handleQuestionaireSave (req: any, res: any) {
         wantsToTravelQuickly,
     } = req.body
 
-    function getAge (birthdate: string) {
-        if (birthdate && birthdate.length) {
-            const today = new Date()
-            const birthDate = new Date(birthdate)
-            let age = today.getFullYear() - birthDate.getFullYear()
-            const m = today.getMonth() - birthDate.getMonth()
-
-            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-                age--
-            }
-
-            return age
-        }
+    const queryData = {
+        hasTraveledTo: null,
+        favouriteHolidayDestination: null,
+        favouriteHolidayTypes: null,
+        plansHolidaysAhead: null,
+        likesToHike: null,
+        prefersInterContinental: null,
+        wantsToVisitSoon: null,
+        hasVisitedThisMuchDestinations: null,
+        favouriteOverallTravelTime: null,
+        wantsToMarry: null,
+        foremostRelationshipMotivation: null,
+        wantsToOrAlreadyHasChildren: null,
+        drinksAlcohol: null,
+        smokes: null,
+        likesToBeInNature: null,
+        favouriteMusicGenre: null,
+        yearlyEarns: null,
+        birthDate: null,
+        livesIn: null,
+        jobTitle: null,
+        lengthInCm: null,
+        description: null,
+        mostImportantInRelationShip: null,
+        wantsToTravelQuickly: null,
+        matchSettings: {
+            matchHasToLikeToBeInNature: null,
+            maxMatchDistance: null,
+            minSearchAge: null,
+            maxSearchAge: null,
+        },
+        age: null,
     }
 
     // Required Fields
     if (
-        (hasTraveledTo && hasTraveledTo.length) &&
-        (favouriteHolidayDestination && favouriteHolidayDestination.length) &&
-        (favouriteHolidayTypes && favouriteHolidayTypes.length) &&
-        (plansHolidaysAhead && plansHolidaysAhead.length) &&
-        (likesToHike && likesToHike.length) &&
-        (prefersInterContinental && prefersInterContinental.length) &&
-        (wantsToVisitSoon && wantsToVisitSoon.length) &&
-        (hasVisitedThisMuchDestinations && hasVisitedThisMuchDestinations.length) &&
-        (favouriteOverallTravelTime && favouriteOverallTravelTime.length) &&
-        (wantsToTravelQuickly && wantsToTravelQuickly.length) &&
-        (likesToBeInNature && likesToBeInNature.length) &&
-        (birthDate && birthDate.length) &&
-        (matchHasToLikeToBeInNature && matchHasToLikeToBeInNature.length) &&
-        (maxMatchDistance && maxMatchDistance.length) &&
-        (minSearchAge && minSearchAge.length) &&
-        (maxSearchAge && maxSearchAge.length)
+        (!hasTraveledTo || !hasTraveledTo.length) ||
+        (!favouriteHolidayDestination || !favouriteHolidayDestination.length) ||
+        (!favouriteHolidayTypes || !favouriteHolidayTypes.length) ||
+        (!plansHolidaysAhead || !plansHolidaysAhead.length) ||
+        (!likesToHike || !likesToHike.length) ||
+        (!prefersInterContinental || !prefersInterContinental.length) ||
+        (!wantsToVisitSoon || !wantsToVisitSoon.length) ||
+        (!hasVisitedThisMuchDestinations || !hasVisitedThisMuchDestinations.length) ||
+        (!favouriteOverallTravelTime || !favouriteOverallTravelTime.length) ||
+        (!wantsToTravelQuickly || !wantsToTravelQuickly.length) ||
+        (!likesToBeInNature || !likesToBeInNature.length) ||
+        (!birthDate || !birthDate.length) ||
+        (!matchHasToLikeToBeInNature || !matchHasToLikeToBeInNature.length) ||
+        (!maxMatchDistance || !maxMatchDistance.length) ||
+        (!minSearchAge || !minSearchAge.length) ||
+        (!maxSearchAge || !maxSearchAge.length)
     ) {
-        queryData[hasTraveledTo] = hasTraveledTo.trim().split(/,?\s+/)
-        queryData[favouriteHolidayDestination] = favouriteHolidayDestination
-        queryData[favouriteHolidayTypes] = favouriteHolidayTypes
-        queryData[plansHolidaysAhead] = plansHolidaysAhead
-        queryData[likesToHike] = likesToHike
-        queryData[prefersInterContinental] = prefersInterContinental
-        queryData[wantsToVisitSoon] = wantsToVisitSoon.trim().split(/,?\s+/)
-        queryData[hasVisitedThisMuchDestinations] = hasVisitedThisMuchDestinations
-        queryData[favouriteOverallTravelTime] = favouriteOverallTravelTime
-        queryData[wantsToTravelQuickly] = wantsToTravelQuickly
-        queryData[likesToBeInNature] = likesToBeInNature
-        queryData[birthDate] = birthDate
-        queryData.age = getAge(birthDate)
-        queryData.matchSettings[matchHasToLikeToBeInNature] = matchHasToLikeToBeInNature
-        queryData.matchSettings[maxMatchDistance] = maxMatchDistance
-        queryData.matchSettings[minSearchAge] = minSearchAge
-        queryData.matchSettings[maxSearchAge] = maxSearchAge
-    } else {
         console.error('Not all required fields of the questionaire are filled in!')
         handleHTTPError(res, 422, 'Unprocessable Entity')
         return
+    } else {
+        queryData.hasTraveledTo = hasTraveledTo.trim().split(/,?\s+/)
+        queryData.favouriteHolidayDestination = favouriteHolidayDestination
+        queryData.favouriteHolidayTypes = favouriteHolidayTypes
+        queryData.plansHolidaysAhead = plansHolidaysAhead
+        queryData.likesToHike = likesToHike
+        queryData.prefersInterContinental = prefersInterContinental
+        queryData.wantsToVisitSoon = wantsToVisitSoon.trim().split(/,?\s+/)
+        queryData.hasVisitedThisMuchDestinations = hasVisitedThisMuchDestinations
+        queryData.favouriteOverallTravelTime = favouriteOverallTravelTime
+        queryData.wantsToTravelQuickly = wantsToTravelQuickly
+        queryData.likesToBeInNature = likesToBeInNature
+        queryData.birthDate = birthDate
+        queryData.age = getAge(birthDate)
+        queryData.matchSettings.matchHasToLikeToBeInNature = matchHasToLikeToBeInNature
+        queryData.matchSettings.maxMatchDistance = maxMatchDistance
+        queryData.matchSettings.minSearchAge = minSearchAge
+        queryData.matchSettings.maxSearchAge = maxSearchAge
     }
 
     // Not required fields
     if (description && description.length) {
-        queryData[description] = description
+        queryData.description = description
     }
 
     if (wantsToMarry && wantsToMarry.length) {
-        queryData[wantsToMarry] = wantsToMarry
+        queryData.wantsToMarry = wantsToMarry
     }
 
     if (foremostRelationshipMotivation && foremostRelationshipMotivation.length) {
-        queryData[foremostRelationshipMotivation] = foremostRelationshipMotivation
+        queryData.foremostRelationshipMotivation = foremostRelationshipMotivation
     }
 
     if (wantsToOrAlreadyHasChildren && wantsToOrAlreadyHasChildren.length) {
-        queryData[wantsToOrAlreadyHasChildren] = wantsToOrAlreadyHasChildren
+        queryData.wantsToOrAlreadyHasChildren = wantsToOrAlreadyHasChildren
     }
 
     if (drinksAlcohol && drinksAlcohol.length) {
-        queryData[drinksAlcohol] = drinksAlcohol
+        queryData.drinksAlcohol = drinksAlcohol
     }
 
     if (smokes && smokes.length) {
-        queryData[smokes] = smokes
+        queryData.smokes = smokes
     }
 
     if (mostImportantInRelationShip && mostImportantInRelationShip.length) {
-        queryData[mostImportantInRelationShip] = mostImportantInRelationShip
+        queryData.mostImportantInRelationShip = mostImportantInRelationShip
     }
 
     if (favouriteMusicGenre && favouriteMusicGenre.length) {
-        queryData[favouriteMusicGenre] = favouriteMusicGenre
+        queryData.favouriteMusicGenre = favouriteMusicGenre
     }
 
     if (yearlyEarns && yearlyEarns.length) {
-        queryData[yearlyEarns] = yearlyEarns
+        queryData.yearlyEarns = yearlyEarns
     }
 
     if (livesIn && livesIn.length) {
-        queryData[livesIn] = livesIn
+        queryData.livesIn = livesIn
     }
 
     if (jobTitle && jobTitle.length) {
-        queryData[jobTitle] = jobTitle
+        queryData.jobTitle = jobTitle
     }
 
     if (lengthInCm && lengthInCm.length) {
-        queryData[lengthInCm] = lengthInCm
+        queryData.lengthInCm = lengthInCm
     }
 
-    Profile.update({ _id }, { $set: queryData })
+    Profile.findOneAndUpdate({ _id }, queryData)
         .exec()
-        .then(result => console.log(result))
+        .then(doc => console.log(doc))
         .catch(error => console.error(error))
 }
 
--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
-import * as multer from 'multer'
+// import * as multer from 'multer'
 import * as mongoose from 'mongoose'
 
 console.log('Re(starting)')
@@ -31,52 +31,52 @@ import deleteAccountController from './controllers/delete_account'
 import deleteChatController from './controllers/delete_chat'
 import questionaireSaveController from './controllers/questionaire_save'
 
-import { FileType } from 'server/types/fileType'
+// import { FileType } from 'server/types/fileType'
 
 const app = express()
 
-// Define that the images need to be stored on a DISK, not in the memory and pass an object with options to it.
-const storage = multer.diskStorage({
-    destination: (request: object, file: FileType, callback: any) => {
-        // Define a place to save the uploaded images.
-        callback(null, 'public/visitorProfileImages/')
-    },
-    filename: (request: object, file: FileType, callback: any) => {
-        const {
-            originalname,
-        } = file
+// // Define that the images need to be stored on a DISK, not in the memory and pass an object with options to it.
+// const storage = multer.diskStorage({
+//     destination: (request: object, file: FileType, callback: any) => {
+//         // Define a place to save the uploaded images.
+//         callback(null, 'public/visitorProfileImages/')
+//     },
+//     filename: (request: object, file: FileType, callback: any) => {
+//         const {
+//             originalname,
+//         } = file
 
-        // Create a good filename to save, by checking if there is a space in the file and replacing it with an underscore.
-        // Then paste the date of creation before it with an underscore and continue the process in the callback.
-        const newDate = new Date().toISOString()
-        const fileNameToSave = originalname.replace(/ /g, '_')
+//         // Create a good filename to save, by checking if there is a space in the file and replacing it with an underscore.
+//         // Then paste the date of creation before it with an underscore and continue the process in the callback.
+//         const newDate = new Date().toISOString()
+//         const fileNameToSave = originalname.replace(/ /g, '_')
 
-        callback(null, `${newDate}_${fileNameToSave}`)
-    },
-})
+//         callback(null, `${newDate}_${fileNameToSave}`)
+//     },
+// })
 
-// Function which filters out the passable filetypes.
-const fileFilter = (request: object, file: FileType, callback: any) => {
-    const {
-        mimetype,
-    } = file
+// // Function which filters out the passable filetypes.
+// const fileFilter = (request: object, file: FileType, callback: any) => {
+//     const {
+//         mimetype,
+//     } = file
 
-    // Check if the mimetype of the passed file matches, what the API accepts.
-    // If it does, continue the flow, else stop the flow and eventually throw an error.
-    if (mimetype === 'image/jpeg' ||
-        mimetype === 'image/jpg' ||
-        mimetype === 'image/png') {
-        callback(null, true)
-    } else {
-        callback(null, false)
-    }
-}
+//     // Check if the mimetype of the passed file matches, what the API accepts.
+//     // If it does, continue the flow, else stop the flow and eventually throw an error.
+//     if (mimetype === 'image/jpeg' ||
+//         mimetype === 'image/jpg' ||
+//         mimetype === 'image/png') {
+//         callback(null, true)
+//     } else {
+//         callback(null, false)
+//     }
+// }
 
-// Upload parameters for uploading files to multer, via FormData.
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-})
+// // Upload parameters for uploading files to multer, via FormData.
+// const upload = multer({
+//     storage: storage,
+//     fileFilter: fileFilter,
+// })
 
 // Setup
 app.set('view engine', 'ejs')
@@ -103,7 +103,7 @@ app.post('/send_message/:_id', sendMessageController)
 app.post('/save_edited_profile', saveEditedProfileController)
 app.post('/delete_account', deleteAccountController)
 app.post('/delete_chat/:_id', deleteChatController)
-app.post('/questionaire_save/:_id', upload.single('profileImage'), questionaireSaveController)
+app.post('/questionaire_save/:_id', questionaireSaveController)
 
 app.use(notFoundRoute)
 
