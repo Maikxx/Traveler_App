ff2aa72bcac57b845a828593187a3134a54adec3
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-20 13:59:05 +0100

    feat(matchProfile): add dynamic data

--- server/routes/match_profile.ts
+++ server/routes/match_profile.ts
@@ -1,38 +1,66 @@
-function renderMatchProfile (req: any, res: any) {
-    const profileData = {
-        _id: 1,
-        firstName: 'Henk',
-        fullName: 'Henk Smits',
-        birthDate: '1980/01/01',
-        ownGender: 'Male',
-        age: 31,
-        profileImages: [ '/visitorProfileImages/available-traveler.jpg', '/visitorProfileImages/available-traveler.jpg', '/visitorProfileImages/available-traveler.jpg', '/visitorProfileImages/available-traveler.jpg' ],
-        description: 'Henk loves to travel, just like you, otherwise you wouldn\'t be here, right?',
-        hasTraveledTo: [ 'Amsterdam', 'Berlin' ],
-        favouriteHolidayDestination: 'Dubai',
-        favouriteHolidayTypes: [ 'Backpacking', 'Family' ],
-        plansHolidaysAhead: 'Sometimes',
-        likesToHike: 'Yes',
-        prefersInterContinental: 'Yes',
-        wantsToVisitSoon: [ 'Dubai', 'Paris' ],
-        hasVisitedThisMuchDestinations: 20,
-        favouriteOverallTravelTime: 3,
-        wantsToTravelQuickly: 'Yes',
-        mostImportantInRelationShip: 'Trust',
-        wantsToMarry: 'Yes',
-        foremostRelationshipMotivation: 'Being able to travel the world with that someone',
-        wantsToOrAlreadyHasChildren: 'Yes, I have them',
-        drinksAlcohol: 'Yes, often',
-        smokes: 'No',
-        likesToBeInNature: 'Yes',
-        favouriteMusicGenre: 'Hardstyle',
-        yearlyEarns: 'More than 50K',
-        livesIn: 'Rotterdam, the Netherlands',
-        jobTitle: 'Architect',
-        lengthInCm: 183,
-    }
+import Profile from '../models/profile'
+import handleHttpError from '../utils/handleError'
 
-    res.render('match_profile.ejs', { profileData })
+function renderMatchProfile (req: any, res: any) {
+    if (req.session && req.session.userId) {
+        const { _id } = req.params
+
+        if (_id) {
+            Profile.find({ _id })
+                .then((result: [ any ]) => {
+                    req.session.error = null
+
+                    const currentResult = result[0]
+
+                    const profileData = {
+                        _id: currentResult._id,
+                        firstName: currentResult.firstName,
+                        fullName: currentResult.fullName,
+                        ownGender: currentResult.ownGender,
+                        birthDate: currentResult.birthDate,
+                        age: currentResult.age,
+                        profileImages: currentResult.profileImages &&
+                            currentResult.profileImages.map(profileImage => profileImage && profileImage.replace('public', '')),
+                        description: currentResult.description,
+                        hasTraveledTo: currentResult.hasTraveledTo,
+                        favouriteHolidayDestination: currentResult.favouriteHolidayDestination,
+                        favouriteHolidayTypes: currentResult.favouriteHolidayTypes,
+                        plansHolidaysAhead: currentResult.plansHolidaysAhead,
+                        likesToHike: currentResult.likesToHike,
+                        prefersInterContinental: currentResult.prefersInterContinental,
+                        wantsToVisitSoon: currentResult.wantsToVisitSoon,
+                        hasVisitedThisMuchDestinations: currentResult.hasVisitedThisMuchDestinations,
+                        favouriteOverallTravelTime: currentResult.favouriteOverallTravelTime,
+                        wantsToTravelQuickly: currentResult.wantsToTravelQuickly,
+                        mostImportantInRelationShip: currentResult.mostImportantInRelationShip,
+                        wantsToMarry: currentResult.wantsToMarry,
+                        foremostRelationshipMotivation: currentResult.foremostRelationshipMotivation,
+                        wantsToOrAlreadyHasChildren: currentResult.wantsToOrAlreadyHasChildren,
+                        drinksAlcohol: currentResult.drinksAlcohol,
+                        smokes: currentResult.smokes,
+                        likesToBeInNature: currentResult.likesToBeInNature,
+                        favouriteMusicGenre: currentResult.favouriteMusicGenre,
+                        yearlyEarns: currentResult.yearlyEarns,
+                        livesIn: currentResult.livesIn,
+                        jobTitle: currentResult.jobTitle,
+                        lengthInCm: currentResult.lengthInCm,
+                    }
+
+                    res.render('match_profile.ejs', { profileData })
+                })
+                .catch(error => {
+                    console.error(error)
+                    console.error('Invalid id passed in!')
+                    handleHttpError(req, res, 400, 'Bad Request', '/matches_overview')
+                })
+        } else {
+            console.error('No id passed in to the url!')
+            handleHttpError(req, res, 400, 'Bad Request', '/matches_overview')
+        }
+    } else {
+        console.error('You are not logged in!')
+        handleHttpError(req, res, 401, 'Credentials Required', '/')
+    }
 }
 
 export default renderMatchProfile
