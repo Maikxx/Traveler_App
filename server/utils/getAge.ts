e784d0cb2e906294f636ce7b7b65b153e968df2a
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-27 23:29:24 +0200

    feat(getAge): add more comments and a mention to a so user

--- server/utils/getAge.ts
+++ server/utils/getAge.ts
@@ -1,12 +1,15 @@
+// Function for getting the age, based on someones birthday.
+// As found (in non TS and ES6+) on StackOverflow by
+// Naveen at https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd
 
 function getAge (birthdate: string) {
     if (birthdate && birthdate.length) {
         const today = new Date()
         const birthDate = new Date(birthdate)
         let age = today.getFullYear() - birthDate.getFullYear()
-        const m = today.getMonth() - birthDate.getMonth()
+        const month = today.getMonth() - birthDate.getMonth()
 
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
             age--
         }
 
