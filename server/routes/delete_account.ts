2219ee0cf8e89e971d00c397de76a65d74c82f60
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-28 22:37:01 +0200

    fix(delete_account): replace destroying session with removing the user

--- server/routes/delete_account.ts
+++ server/routes/delete_account.ts
@@ -1,78 +1,76 @@
 import * as fs from 'fs'
 import * as express from 'express'
+import * as mongoose from 'mongoose'
+
 import Profile from '../models/profile'
-import handleHttpError from '../utils/handleError'
+
 import { SessionType } from '../types/SessionType'
 import { ProfileType } from '../types/ProfileType'
+import { CustomErrorType } from '../types/customErrorType'
+
+import handleHttpError from '../utils/handleError'
+
+// Todo make this a DELETE route, instead of a get.
+
+/*
+Route for deleting users.
+
+1. THe code looks for a valid profile and gets it from the database.
+2. Delete all the profile images of this user, if he has any.
+3. Delete the users account.
+*/
 
 function handleDeleteAccount (req: express.Request & {session: SessionType}, res: express.Response) {
+    const cusErr = {
+        redirectTo: '/',
+        scope: 'delete_account',
+        message: '',
+        logOut: true,
+    }
+
     if (req.session && req.session.userId) {
         Profile.findOne({ _id: req.session.userId })
             .then((result: ProfileType) => {
-                if (result) {
-                    const { profileImages } = result
+                const { profileImages } = result
 
-                    if (profileImages && profileImages.length) {
-                        profileImages.forEach((image) => {
-                            fs.unlink(image, (error) => {
-                                if (error) {
-                                    console.error(error)
-                                    handleHttpError(
-                                        req,
-                                        res,
-                                        500,
-                                        '/',
-                                        'delete_account',
-                                        'There was an error unlinking an image!',
-                                        true,
-                                        error
-                                    )
-                                }
-                            })
-                        })
-
-                        req.session.destroy(error => {
+                if (profileImages && profileImages.length) {
+                    profileImages.forEach((imagePath: string) => {
+                        fs.unlink(imagePath, (error: NodeJS.ErrnoException) => {
                             if (error) {
-                                handleHttpError(
-                                    req,
-                                    res,
-                                    500,
-                                    '/',
-                                    'delete_account',
-                                    'There was an error while destroying a session!',
-                                    true,
-                                    error
-                                )
-                            } else {
-                                res.status(200).redirect('/')
+                                cusErr.message = 'There was an error unlinking an image!'
+
+                                handleHttpError(req, res, 500, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut, error)
                             }
                         })
-                    }
+                    })
+
+                    deleteAccount(req, res, cusErr, req.session.userId)
+                } else {
+                    deleteAccount(req, res, cusErr, req.session.userId)
                 }
             })
-            .catch(error => {
-                handleHttpError(
-                    req,
-                    res,
-                    401,
-                    '/',
-                    'delete_account',
-                    'No such user exists!',
-                    true,
-                    error
-                )
+            .catch((error: mongoose.Error) => {
+                cusErr.message = 'No such user exists!'
+
+                handleHttpError(req, res, 401, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut, error)
             })
     } else {
-        console.error('You are not logged in!')
-        handleHttpError(
-            req,
-            res,
-            401,
-            '/',
-            'delete_account',
-            'You are not logged in!'
-        )
+        cusErr.message = 'You are not logged in!'
+
+        handleHttpError(req, res, 401, cusErr.redirectTo, cusErr.scope, cusErr.message, true)
     }
+}
+
+function deleteAccount(req: express.Request & {session: SessionType}, res: express.Response, cusErr: CustomErrorType, userId: string) {
+    Profile.remove({ _id: userId })
+        .then(() => {
+            res.status(204).redirect('/')
+        })
+        .catch((error: mongoose.Error) => {
+            cusErr.message = 'There was an error while deleting your account!'
+
+            handleHttpError(req, res, 500, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut, error)
+        })
 }
 
 export default handleDeleteAccount
---
+++ server/types/customErrorType.ts
@@ -0,0 +1,6 @@
+export interface CustomErrorType {
+    redirectTo: string
+    scope: string
+    message: string
+    logOut: boolean
+}
