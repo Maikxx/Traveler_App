21222b0103ac12842af6a54f5b5ea0202c92e6af
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-28 16:42:49 +0200

    feat(sign_up): add more code clarity

--- server/controllers/sign_up.ts
+++ server/controllers/sign_up.ts
@@ -1,11 +1,30 @@
 import * as express from 'express'
 import * as mongoose from 'mongoose'
 import * as bcrypt from 'bcrypt'
-import handleHttpError from '../utils/handleError'
+
 import Profile from '../models/profile'
+
 import { SessionType } from '../types/SessionType'
+import { ProfileType } from 'server/types/ProfileType'
+
+import handleHttpError from '../utils/handleError'
+
+/*
+Function that handles signing new users up.
+
+The data gets tested, before it is stored in Mongo.
+There are checks to see if a Profile already exists, if the data is valid and if the user filled in everything the right way.
+If everything succeeds the new user will be stored in the database and the user will be redirected to the questionaire page.
+*/
 
 function handleSignUp (req: express.Request & {session: SessionType}, res: express.Response) {
+    const customError = {
+        redirectTo: '/',
+        scope: 'sign_up',
+        message: '',
+        logOut: false,
+    }
+
     const emailRegex = /^\w+@\w+\..{2,3}(.{2,3})?$/
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,}$/
 
@@ -19,85 +38,71 @@ function handleSignUp (req: express.Request & {session: SessionType}, res: expre
             lookingForGender,
         } = req.body
 
-        if (email.length && emailRegex.test(email)) {
+        if (
+            email && email.length && emailRegex.test(email) &&
+            fullName && fullName.length &&
+            password && password.length && passwordRegex.test(password) && repeatPassword === password &&
+            ownGender && ownGender.length &&
+            lookingForGender && lookingForGender.length
+        ) {
             Profile.find({ email: email.toLowerCase() })
                 .exec()
-                .then((user: any) => {
-                    if (user.length) {
-                        handleHttpError(
-                            req,
-                            res,
-                            409,
-                            '/',
-                            'sign_up',
-                            'Mail already exists!'
-                        )
-                    } else {
-                        bcrypt.hash(req.body.password, 10, (error: any, hash: string) => {
-                            if (error) {
-                                handleHttpError(
-                                    req,
-                                    res,
-                                    400,
-                                    '/',
-                                    'sign_up',
-                                    'Uncaught error!',
-                                    true,
-                                    error
-                                )
-                            } else {
-                                if (
-                                    fullName && fullName.length &&
-                                    password && password.length && passwordRegex.test(password) && repeatPassword === password &&
-                                    ownGender && ownGender.length &&
-                                    lookingForGender && lookingForGender.length
-                                ) {
-                                    const newUser = new Profile({
-                                        _id: new mongoose.Types.ObjectId(),
-                                        fullName,
-                                        firstName: fullName && fullName.length && fullName.substr(0, fullName.indexOf(' ')),
-                                        email: email.toLowerCase(),
-                                        password: hash,
-                                        ownGender,
-                                        matchSettings: {
-                                            lookingForGender,
-                                        },
-                                    })
+                .then((user: ProfileType[]) => {
+                    if (user && user.length) {
+                        customError.message = 'Mail already exists!'
 
-                                    newUser.save()
-                                        .then(result => {
-                                            req.session.userId = result._id
-                                            req.session.error = null
-                                            res.status(200).redirect(`/questionaire`)
-                                        })
-                                        .catch(error => {
-                                            console.error(error)
-                                            handleHttpError(
-                                                req,
-                                                res,
-                                                500,
-                                                '/',
-                                                'sign_up',
-                                                'Error while saving a new user!',
-                                                true,
-                                                error
-                                            )
-                                        })
-                                }
+                        handleHttpError(req, res, 409, customError.redirectTo, customError.scope, customError.message, false)
+                    } else {
+                        bcrypt.hash(req.body.password, 10, (error: NodeJS.ErrnoException, hash: string) => {
+                            if (error) {
+                                customError.message = 'Uncaught error!'
+
+                                handleHttpError(req, res, 400, customError.redirectTo,
+                                    customError.scope, customError.message, customError.logOut, error)
+                            } else {
+                                const newUser = new Profile({
+                                    _id: new mongoose.Types.ObjectId(),
+                                    fullName,
+                                    firstName: fullName && fullName.length && fullName.substr(0, fullName.indexOf(' ')),
+                                    email: email.toLowerCase(),
+                                    password: hash,
+                                    ownGender,
+                                    matchSettings: {
+                                        lookingForGender,
+                                    },
+                                })
+
+                                newUser.save()
+                                    .then((result: ProfileType) => {
+                                        req.session.userId = result._id
+                                        req.session.error = null
+                                        res.status(200).redirect(`/questionaire`)
+                                    })
+                                    .catch((error: mongoose.NativeError) => {
+                                        customError.message = 'Error while saving a new user!'
+
+                                        handleHttpError(req, res, 500, customError.redirectTo,
+                                            customError.scope, customError.message, customError.logOut, error)
+                                    })
                             }
                         })
                     }
                 })
+                .catch((error: mongoose.NativeError) => {
+                    customError.message = 'Something went wrong with getting your mail!'
+
+                    handleHttpError(req, res, 500, customError.redirectTo,
+                        customError.scope, customError.message, customError.logOut, error)
+                })
         } else {
-            handleHttpError(
-                req,
-                res,
-                422,
-                '/',
-                'sign_up',
-                'Mail is not valid!'
-            )
+            customError.message = 'Mail is not valid!'
+
+            handleHttpError(req, res, 422, customError.redirectTo, customError.scope, customError.message, customError.logOut)
         }
+    } else {
+        customError.message = 'We did not receive any form data from you! Please try again!'
+
+        handleHttpError(req, res, 400, customError.redirectTo, customError.scope, customError.message, customError.logOut)
     }
 }
 
