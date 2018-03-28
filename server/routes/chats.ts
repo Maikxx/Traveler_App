d507fab4266cb87ae8140b846601da979ed5e267
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-28 19:04:26 +0200

    refactor(chats): shorten the code

--- server/routes/chats.ts
+++ server/routes/chats.ts
@@ -1,10 +1,14 @@
 import * as express from 'express'
+import * as mongoose from 'mongoose'
+
 import Profile from '../models/profile'
 import Chat from '../models/chat'
+
 import { SessionType } from '../types/SessionType'
+import { ProfileType } from '../types/profileType'
+import { ChatType } from '../types/chatType'
+
 import handleHttpError from '../utils/handleError'
-import { ProfileType } from 'server/types/profileType'
-import { ChatType } from 'server/types/chatType'
 
 function renderChats (req: express.Request & {session: SessionType}, res: express.Response) {
     req.session.error = null
@@ -28,73 +32,32 @@ function renderChats (req: express.Request & {session: SessionType}, res: expres
                                                     && chatWithProfile.profileImages.length
                                                     && chatWithProfile.profileImages[0].replace('public', ''),
                                             }))
-                                            .catch(error => {
-                                                handleHttpError(
-                                                    req,
-                                                    res,
-                                                    500,
-                                                    '/',
-                                                    'chats',
-                                                    'Something went wrong with getting the profile of a chat!',
-                                                    true,
-                                                    error
-                                                )
+                                            .catch((error: mongoose.Error) => {
+                                                // tslint:disable-next-line:ter-max-len
+                                                handleHttpError(req, res, 500, '/', 'chats', 'Something went wrong with getting the profile of a chat!', false, error)
                                             })
                                     }
                                 }
                             })
-                            .catch(error => {
-                                handleHttpError(
-                                    req,
-                                    res,
-                                    500,
-                                    '/',
-                                    'chats',
-                                    'Something went wrong with getting the profile of a chat!',
-                                    true,
-                                    error
-                                )
+                            .catch((error: mongoose.Error) => {
+                                // tslint:disable-next-line:ter-max-len
+                                handleHttpError(req, res, 500, '/', 'chats', 'Something went wrong with getting the profile of a chat!', false, error)
                             })
                     }))
                         .then(rawData => {
                             const openChatsData = rawData.filter(data => data !== undefined)
                             res.status(200).render('chats.ejs', { openChatsData })
                         })
-                        .catch(error => {
-                            handleHttpError(
-                                req,
-                                res,
-                                500,
-                                '/',
-                                'chats',
-                                'Boiled up error in the Promise.all!',
-                                true,
-                                error
-                            )
+                        .catch((error: mongoose.Error) => {
+                            handleHttpError(req, res, 500, '/', 'chats', 'Boiled up error in the Promise.all!', false, error)
                         })
                 }
             })
-            .catch(error => {
-                handleHttpError(
-                    req,
-                    res,
-                    500,
-                    '/',
-                    'chats',
-                    'Invalid Own User ID!',
-                    true,
-                    error
-                )
+            .catch((error: mongoose.Error) => {
+                handleHttpError(req, res, 500, '/', 'chats', 'Invalid Own User ID!', false, error)
             })
     } else {
-        handleHttpError(
-            req,
-            res,
-            403,
-            '/',
-            'chats',
-            'You need to be logged in to view your chats!'
-        )
+        handleHttpError(req, res, 403, '/', 'chats', 'You need to be logged in to view your chats!')
     }
 }
 
