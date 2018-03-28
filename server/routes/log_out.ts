885308ee922c64fe6798fb77ffad90858bffe75d
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-28 22:44:17 +0200

    refactor(log_out): add comments and shuffle some things around

--- server/routes/log_out.ts
+++ server/routes/log_out.ts
@@ -1,24 +1,37 @@
 import * as express from 'express'
-import handleHttpError from '../utils/handleError'
+
 import { SessionType } from '../types/SessionType'
 
+import handleHttpError from '../utils/handleError'
+
+/*
+Route for loggin users out.
+
+1. Destroy a users session.
+2. Redirect the user back to the homepage.
+*/
+
 function handleLogOut (req: express.Request & {session: SessionType}, res: express.Response) {
-    req.session.destroy(error => {
-        if (error) {
-            handleHttpError(
-                req,
-                res,
-                500,
-                '/',
-                'log_out',
-                'There was an error logging you out!',
-                true,
-                error
-            )
-        } else {
-            res.redirect('/')
-        }
-    })
+    const cusErr = {
+        redirectTo: '/',
+        scope: 'log_out',
+        message: '',
+        logOut: true,
+    }
+
+    if (req.session) {
+        req.session.destroy((error: NodeJS.ErrnoException) => {
+            if (error) {
+                cusErr.message = 'There was an error logging you out!'
+
+                handleHttpError(req, res, 500, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut, error)
+            } else {
+                res.status(204).redirect('/')
+            }
+        })
+    } else {
+        res.status(409).redirect('/')
+    }
 }
 
 export default handleLogOut
