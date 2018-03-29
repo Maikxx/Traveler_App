ac2eaca354b0b3476c024c163b718e7eb7ea9a95
Maikel van Veen <maikeloo10@hotmail.com>
2018-03-29 13:13:55 +0200

    fix(*): fix several breaking things

--- public/index.js
+++ public/index.js
@@ -1,32 +1,24 @@
-// To do: Put this file in source and let it be automatically generated and transpiled.
-
-// Function that handles clicks on the menu icon in the top right of pages where this icon is present.
-const handleMenuClick = (() => {
-    const menuToggle = document.getElementById('menu-toggle')
-    const navigation = document.getElementById('tl-Navigation')
-
-    if (menuToggle && navigation) {
-        navigation.classList.add('js-active')
-        menuToggle.addEventListener('click', function () {
-            navigation.classList.toggle('nav-toggled')
-        })
-    }
-})()
-
-// Function that handles clicking the textbutton below the sign up form, to open the sign in window.
-const handleToggleSignInClick = (() => {
-    const toggleSignIn = document.getElementById('toggle-sign-in-button')
-    const navigation = document.getElementById('tl-Navigation')
-
-    if (toggleSignIn && navigation) {
-        toggleSignIn.addEventListener('click', function () {
-
-            window.location.hash === '#tl-Navigation' ? (
-                window.location.hash = '',
-                window.location.hash = 'tl-Navigation'
-            ) : window.location.hash = 'tl-Navigation'
-
-            navigation.classList.toggle('nav-toggled')
-        })
-    }
-})()
+"use strict";
+var handleMenuClick = (function() {
+    var a = document.getElementById("menu-toggle"),
+      b = document.getElementById("tl-Navigation");
+    a &&
+      b &&
+      (b.classList.add("js-active"),
+      a.addEventListener("click", function() {
+        b.classList.toggle("nav-toggled");
+      }));
+  })(),
+  handleToggleSignInClick = (function() {
+    var a = document.getElementById("toggle-sign-in-button"),
+      b = document.getElementById("tl-Navigation");
+    a &&
+      b &&
+      a.addEventListener("click", function() {
+        "#tl-Navigation" === window.location.hash
+          ? ((window.location.hash = ""),
+            (window.location.hash = "tl-Navigation"))
+          : (window.location.hash = "tl-Navigation"),
+          b.classList.toggle("nav-toggled");
+      });
+  })();
---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+// Function that handles clicks on the menu icon in the top right of pages where this icon is present.
+const handleMenuClick = (() => {
+    const menuToggle = document.getElementById('menu-toggle')
+    const navigation = document.getElementById('tl-Navigation')
+
+    if (menuToggle && navigation) {
+        navigation.classList.add('js-active')
+        menuToggle.addEventListener('click', function () {
+            navigation.classList.toggle('nav-toggled')
+        })
+    }
+})()
+
+// Function that handles clicking the textbutton below the sign up form, to open the sign in window.
+const handleToggleSignInClick = (() => {
+    const toggleSignIn = document.getElementById('toggle-sign-in-button')
+    const navigation = document.getElementById('tl-Navigation')
+
+    if (toggleSignIn && navigation) {
+        toggleSignIn.addEventListener('click', function () {
+
+            window.location.hash === '#tl-Navigation' ? (
+                window.location.hash = '',
+                window.location.hash = 'tl-Navigation'
+            ) : window.location.hash = 'tl-Navigation'
+
+            navigation.classList.toggle('nav-toggled')
+        })
+    }
+})()
