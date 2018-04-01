6c1c92652bb575f0f3f0746db6153660eac57961
Maikel van Veen <maikeloo10@hotmail.com>
2018-04-01 15:25:44 +0200

    fix(sign_up): remove unneeded check

--- server/controllers/sign_up.ts
+++ server/controllers/sign_up.ts
@@ -27,81 +27,75 @@ function handleSignUp (req: express.Request & {session: SessionType}, res: expre
         logOut: false,
     }
 
-    if (req.body) {
-        const {
-            fullName,
-            email,
-            password,
-            repeatPassword,
-            ownGender,
-            lookingForGender,
-        } = req.body
+    const {
+        fullName,
+        email,
+        password,
+        repeatPassword,
+        ownGender,
+        lookingForGender,
+    } = req.body
 
-        if (
-            email && email.length && validationRegex.email.test(email) &&
-            fullName && fullName.length &&
-            password && password.length && validationRegex.password.test(password) && repeatPassword === password &&
-            ownGender && ownGender.length &&
-            lookingForGender && lookingForGender.length
-        ) {
-            Profile.find({ email: email.toLowerCase() })
-                .exec()
-                .then((user: ProfileType[]) => {
-                    if (user && user.length) {
-                        cusErr.message = 'Mail already exists!'
+    if (
+        email && email.length && validationRegex.email.test(email) &&
+        fullName && fullName.length &&
+        password && password.length && validationRegex.password.test(password) && repeatPassword === password &&
+        ownGender && ownGender.length &&
+        lookingForGender && lookingForGender.length
+    ) {
+        Profile.find({ email: email.toLowerCase() })
+            .exec()
+            .then((user: ProfileType[]) => {
+                if (user && user.length) {
+                    cusErr.message = 'Mail already exists!'
 
-                        handleHttpError(req, res, 409, cusErr.redirectTo, cusErr.scope, cusErr.message, false)
-                    } else {
-                        bcrypt.hash(req.body.password, 10, (error: NodeJS.ErrnoException, hash: string) => {
-                            if (error) {
-                                handleHttpError(req, res, 500, cusErr.redirectTo,
-                                    cusErr.scope, errorMessages.serverError, cusErr.logOut, error)
-                            } else {
-                                const newUser = new Profile({
-                                    _id: new mongoose.Types.ObjectId(),
-                                    fullName,
-                                    firstName: fullName && fullName.length && fullName.substr(0, fullName.indexOf(' ')),
-                                    email: email.toLowerCase(),
-                                    password: hash,
-                                    ownGender,
-                                    matchSettings: {
-                                        lookingForGender,
-                                    },
-                                    hasFinishedQuestionaire: false,
+                    handleHttpError(req, res, 409, cusErr.redirectTo, cusErr.scope, cusErr.message, false)
+                } else {
+                    bcrypt.hash(req.body.password, 10, (error: NodeJS.ErrnoException, hash: string) => {
+                        if (error) {
+                            handleHttpError(req, res, 500, cusErr.redirectTo,
+                                cusErr.scope, errorMessages.serverError, cusErr.logOut, error)
+                        } else {
+                            const newUser = new Profile({
+                                _id: new mongoose.Types.ObjectId(),
+                                fullName,
+                                firstName: fullName && fullName.length && fullName.substr(0, fullName.indexOf(' ')),
+                                email: email.toLowerCase(),
+                                password: hash,
+                                ownGender,
+                                matchSettings: {
+                                    lookingForGender,
+                                },
+                                hasFinishedQuestionaire: false,
+                            })
+
+                            newUser.save()
+                                .then((result: ProfileType) => {
+                                    req.session.userId = result._id
+                                    req.session.error = null
+
+                                    res.status(200).redirect(`/questionaire`)
                                 })
+                                .catch((error: mongoose.Error) => {
+                                    cusErr.message = 'Error while saving a new user!'
 
-                                newUser.save()
-                                    .then((result: ProfileType) => {
-                                        req.session.userId = result._id
-                                        req.session.error = null
+                                    handleHttpError(req, res, 500, cusErr.redirectTo,
+                                        cusErr.scope, cusErr.message, cusErr.logOut, error)
+                                })
+                        }
+                    })
+                }
+            })
+            .catch((error: mongoose.Error) => {
+                cusErr.message = 'Something went wrong with getting your mail!'
 
-                                        res.status(200).redirect(`/questionaire`)
-                                    })
-                                    .catch((error: mongoose.Error) => {
-                                        cusErr.message = 'Error while saving a new user!'
-
-                                        handleHttpError(req, res, 500, cusErr.redirectTo,
-                                            cusErr.scope, cusErr.message, cusErr.logOut, error)
-                                    })
-                            }
-                        })
-                    }
-                })
-                .catch((error: mongoose.Error) => {
-                    cusErr.message = 'Something went wrong with getting your mail!'
-
-                    handleHttpError(req, res, 500, cusErr.redirectTo,
-                        cusErr.scope, cusErr.message, cusErr.logOut, error)
-                })
-        } else {
-            cusErr.message = 'Mail is not valid!'
-
-            handleHttpError(req, res, 422, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut)
-        }
+                handleHttpError(req, res, 500, cusErr.redirectTo,
+                    cusErr.scope, cusErr.message, cusErr.logOut, error)
+            })
     } else {
-        cusErr.message = 'We did not receive any form data from you! Please try again!'
+        cusErr.message = 'Mail is not valid!'
 
-        handleHttpError(req, res, 400, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut)
+        handleHttpError(req, res, 422, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut)
     }
 }
 
