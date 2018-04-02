c1e1e86083a51a798287883286e33b0a84bc9098
Maikel van Veen <maikeloo10@hotmail.com>
2018-04-02 14:14:16 +0200

    refactor(matches_overview): async awaitify

--- server/routes/matches_overview.ts
+++ server/routes/matches_overview.ts
@@ -1,12 +1,9 @@
 import * as express from 'express'
-import * as mongoose from 'mongoose'
 
 import Profile from '../models/profile'
 
 import { SessionType } from '../types/SessionType'
 import { ProfileType } from '../types/ProfileType'
-
-import handleHttpError from '../utils/handleError'
 
 /*
 Route for showing the user his or her matches, based on the data they filled in.
@@ -24,65 +21,56 @@ Route for showing the user his or her matches, based on the data they filled in.
 4. Render that data to the page for the user.
 */
 
-function renderMatchesOverview (req: express.Request & {session: SessionType}, res: express.Response) {
+async function renderMatchesOverview (req: express.Request & {session: SessionType}, res: express.Response, next: express.NextFunction) {
     const cusErr = {
+        req,
+        res,
+        code: 401,
         redirectTo: '/',
         scope: 'matches_overview',
-        message: '',
+        message: 'You need to be logged in to view your matches!',
         logOut: true,
     }
 
     if (req.session && req.session.userId) {
-        Profile.findOne({ _id: req.session.userId })
-            .then((myProfile: ProfileType) => {
-                if (!myProfile.hasFinishedQuestionaire) {
-                    cusErr.message = 'You have not yet filled in the questionaire!'
+        try {
+            const myProfile = await Profile.findOne({ _id: req.session.userId }) as ProfileType
 
-                    handleHttpError(req, res, 403, '/questionaire', cusErr.scope, cusErr.message, cusErr.logOut)
-                } else {
-                    Profile.find({ _id: { $ne: myProfile._id } })
-                        .where('ownGender', myProfile.matchSettings.lookingForGender)
-                        .where('age').gte(myProfile.matchSettings.minSearchAge).lte(myProfile.matchSettings.maxSearchAge)
-                        .where('matchSettings.lookingForGender', myProfile.ownGender)
-                        .where('matchSettings.minSearchAge').gte(myProfile.age)
-                        // .where('matchSettings.maxSearchAge').lte(myProfile.age)
-                        .limit(10)
-                        .then((matchedProfiles: ProfileType[]) => {
-                            req.session.error = null
+            if (!myProfile.hasFinishedQuestionaire) {
+                throw new Error('You have not yet filled in the questionaire!')
+            } else {
+                const matchedProfiles = await Profile.find({ _id: { $ne: myProfile._id } })
+                    .where('ownGender', myProfile.matchSettings.lookingForGender)
+                    .where('age').gte(myProfile.matchSettings.minSearchAge).lte(myProfile.matchSettings.maxSearchAge)
+                    .where('matchSettings.lookingForGender', myProfile.ownGender)
+                    .where('matchSettings.minSearchAge').gte(myProfile.age)
+                    // .where('matchSettings.maxSearchAge').lte(myProfile.age)
+                    .limit(10) as ProfileType[]
 
-                            const overviewData = matchedProfiles.map((matchedProfile: ProfileType) => ({
-                                _id: matchedProfile._id,
-                                firstName: matchedProfile.firstName,
-                                fullName: matchedProfile.fullName,
-                                profileImages: matchedProfile.profileImages &&
-                                    matchedProfile.profileImages.length &&
-                                    matchedProfile.profileImages.map(profileImage => profileImage.replace('public', '')),
-                                age: matchedProfile.age,
-                                ownGender: matchedProfile.ownGender,
-                                description: matchedProfile.description,
-                                favouriteHolidayDestination: matchedProfile.favouriteHolidayDestination,
-                                likesToHike: matchedProfile.likesToHike,
-                                favouriteOverallTravelTime: matchedProfile.favouriteOverallTravelTime,
-                            }))
+                req.session.error = null
 
-                            res.render('matches_overview.ejs', { overviewData })
-                        })
-                        .catch((error: mongoose.Error) => {
-                            cusErr.message = 'Error while finding a profile!'
+                const overviewData = matchedProfiles.map((matchedProfile: ProfileType) => ({
+                    _id: matchedProfile._id,
+                    firstName: matchedProfile.firstName,
+                    fullName: matchedProfile.fullName,
+                    profileImages: matchedProfile.profileImages &&
+                        matchedProfile.profileImages.length &&
+                        matchedProfile.profileImages.map(profileImage => profileImage.replace('public', '')),
+                    age: matchedProfile.age,
+                    ownGender: matchedProfile.ownGender,
+                    description: matchedProfile.description,
+                    favouriteHolidayDestination: matchedProfile.favouriteHolidayDestination,
+                    likesToHike: matchedProfile.likesToHike,
+                    favouriteOverallTravelTime: matchedProfile.favouriteOverallTravelTime,
+                }))
 
-                            handleHttpError(req, res, 500, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut, error)
-                        })
-                }
-            })
-            .catch((error: mongoose.Error) => {
-                cusErr.message = 'Error finding your profile!'
-
-                handleHttpError(req, res, 500, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut, error)
-            })
+                res.render('matches_overview.ejs', { overviewData })
+            }
+        } catch (error) {
+            next(error)
+        }
     } else {
-        cusErr.message = 'You are not logged in!'
-
-        handleHttpError(req, res, 401, cusErr.redirectTo, cusErr.scope, cusErr.message, cusErr.logOut)
+        next(cusErr)
     }
 }
 
